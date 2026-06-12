4fb7bdb46dbe85693ac11b74f819e3570b287492
Krishika Sahni <106904431+krishikasahni@users.noreply.github.com>
2026-06-13 02:24:42 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,46 +1,26 @@
-print("STEP 1")
-
 from analytics.dashboard import (
     get_top_queries,
     get_top_clicked_docs
 )
 
-print("STEP 2")
-
 from analytics.logger import log_query
-
-print("STEP 3")
 
 from ranking.hybrid_ranker import hybrid_rank
 
-print("STEP 4")
-
 from analytics.stats import get_analytics
-
-print("STEP 5")
 
 from feedback.feedback import save_feedback
 
-print("STEP 6")
-
 from search.autocomplete import get_suggestions
-
-print("STEP 7")
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-print("STEP 8")
-
 import json
 import os
 
-print("STEP 9")
-
 app = FastAPI()
-
-print("STEP 10")
 
 app.add_middleware(
     CORSMiddleware,
@@ -60,8 +40,6 @@ print("STEP 11")
 with open(DATA_PATH, "r") as f:
     DOCUMENTS = json.load(f)
 
-print("STEP 12")
-
 class SearchRequest(BaseModel):
     query: str
 
@@ -69,17 +47,11 @@ class FeedbackRequest(BaseModel):
     query: str
     title: str
 
-print("STEP 13")
-
 @app.get("/")
 def home():
     return {"message": "Search Engine Backend Running"}
 
-print("STEP 14")
-
 #from semantic.semantic_search import semantic_search
-
-print("STEP 15")
 
 @app.post("/search")
 def search(request: SearchRequest):
